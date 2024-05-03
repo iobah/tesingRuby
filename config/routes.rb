c149d2f6abaf080a889fe67c7d390e0bd0fb1c31
Andrea Caselli <andrea.caselli@iit.it>
2024-05-03 16:38:05 +0200

    modified button delete

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root "pages#home"
   get "about", to: "pages#about"
-  resources :articles, only: [:show]
+  resources :articles
   # Defines the root path route ("/")
   # root "posts#index"
 end
